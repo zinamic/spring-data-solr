999740a9e80ea4ab83021d831b2cf3357305e27a
Jose L. Hernandez <jose@rocklobster>
2024-10-23 13:14:15 +0200

    revert all dependency upgrades except Java 21

--- src/main/java/org/springframework/data/solr/repository/query/SolrParameter.java
+++ src/main/java/org/springframework/data/solr/repository/query/SolrParameter.java
@@ -18,7 +18,6 @@ package org.springframework.data.solr.repository.query;
 import org.springframework.core.MethodParameter;
 import org.springframework.data.repository.query.Parameter;
 import org.springframework.data.solr.repository.Boost;
-import org.springframework.data.util.TypeInformation;
 import org.springframework.lang.Nullable;
 
 /**
@@ -30,8 +29,8 @@ class SolrParameter extends Parameter {
 
 	private final MethodParameter parameter;
 
-	protected SolrParameter(MethodParameter parameter, TypeInformation<?> domainType) {
-		super(parameter, domainType);
+	protected SolrParameter(MethodParameter parameter) {
+		super(parameter);
 		this.parameter = parameter;
 	}
 
